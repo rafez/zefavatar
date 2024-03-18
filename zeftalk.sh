82525ee3b3932af4db4aceed65d1dfdfcbb62d43
rafez <rafeznoorullah@gmail.com>
2024-03-18 12:35:09 +0000

    2024-03-18T11:32:38

--- zeftalk.sh
+++ zeftalk.sh
@@ -1,2 +1,2 @@
-#!/bash/etc
+#!/bin/bash
            !python3.8 inference.py --driven_audio ./data/2024-03-18T11:32:38/output.wav            --source_image ./img/zef_trim.png            --result_dir ./data/2024-03-18T11:32:38 --preprocess full --enhancer gfpgan
