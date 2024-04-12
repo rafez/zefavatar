d56213214e7b798c97a54081b0382e06f528f2aa
Rafez Noorullah <rafeznoorullah@gmail.com>
2024-04-12 01:23:52 +0100

    Added Bengali

---
+++ zeftalk-bn.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+           python3.8 inference.py --driven_audio ./data/zef-birth-bn.wav            --source_image ./img/zef_trim.png            --result_dir ./data/ --preprocess full --enhancer gfpgan
